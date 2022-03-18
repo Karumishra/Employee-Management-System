67a940e1783956921c88a56f3ed049f072aaa926
Karunakar Mishra <42409280+Karumishra@users.noreply.github.com>
2022-03-18 21:34:49 +0530

    Delete Employee_Management_System directory

--- Employee_Management_System/src/main/java/com/example/sprinboot/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sprinboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sprinboot.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
