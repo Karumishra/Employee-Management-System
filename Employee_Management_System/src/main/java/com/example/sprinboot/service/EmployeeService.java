23944c09b6ce449a4c21d2585917217d03739e54
Karunakar Mishra <42409280+Karumishra@users.noreply.github.com>
2022-03-18 21:36:10 +0530

    Add files via upload

---
+++ Employee_Management_System/src/main/java/com/example/sprinboot/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.sprinboot.service;
+
+import java.util.List;
+
+import com.example.sprinboot.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> getAllEmployees();
+	
+	Employee saveEmployee(Employee employee);
+	
+	Employee getEmployeeById(Long id);
+	
+	Employee updateEmployee(Employee employee);
+	
+	void deleteEmployeeById(Long id);
+}
